6fccb33d6cc4a63c7d96536ffa206db1206f4623
Miguel Oliveira <miguel.gazela@gmail.com>
2016-05-06 14:43:42 +0100

    Added boilerplate view controllers

---
+++ Flickretch/Controllers/MGContactsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGContactsTableViewController.h
+//  Flickretch
+//
+//  Created by Miguel Oliveira on 06/05/16.
+//  Copyright © 2016 Miguel Oliveira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGContactsTableViewController : UITableViewController
+
+@end
